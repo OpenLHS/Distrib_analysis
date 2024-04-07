ffaf7e929e467a2601ba603c53ce446dbde8cd87
jf <ethierjf@users.noreply.github.com>
2024-04-06 22:24:20 -0400

    Update for manual wd set option in log reg

--- logistic_regression/examples/random_data_same_folder/distributed/Data_node_call_iter_log-reg.R
+++ logistic_regression/examples/random_data_same_folder/distributed/Data_node_call_iter_log-reg.R
@@ -8,36 +8,40 @@
 # Currently, the automated node number allocation currently requires execution in R studio and rstudioapi package
 # https://cran.r-project.org/package=rstudioapi
 
-data_call_iter_log_reg <- function(man_nodeid=-1, man_iter=-1) {
+data_call_iter_log_reg <- function(man_wd=-1,man_nodeid=-1, man_iter=-1) {
 
-# If you want to override the node numbering based on filename, input 0 or a positive integer here
+manualwd <- man_wd
 manualk <- man_nodeid
 manualt <- man_iter
 
 # No modifications should be required below this point
 ###########################
 
-k <- -1
-t <- -1
-
-# Set working directory automatically
-
-# this.path package is available
-if (require(this.path)) {
-  setwd(this.dir())
+if (manualwd != 1) {
   
-  # else if running in R studio and the rstudioapi is available, set the correct working directory
-} else if ((Sys.getenv("RSTUDIO") == "1") & (require("rstudioapi"))) {
-  print("RSTUDIO")
-  path <- dirname(rstudioapi::getActiveDocumentContext()$path)
-  setwd(path)
+  # Set working directory automatically
   
-  # no known means to automatically set working directory
+  # this.path package is available
+  if (require(this.path)) {
+    setwd(this.dir())
+    
+    # else if running in R studio and the rstudioapi is available, set the correct working directory
+  } else if ((Sys.getenv("RSTUDIO") == "1") & (require("rstudioapi"))) {
+    print("RSTUDIO")
+    path <- dirname(rstudioapi::getActiveDocumentContext()$path)
+    setwd(path)
+    
+    # no known means to automatically set working directory
+  } else {
+    stop("The required conditions to automatically set the working directory are not met. See R file")
+  }
 } else {
-  stop("The required conditions to automatically set the working directory are not met. See R file")
+  print("The automated working directory setup has been bypassed. If there is an error, this might be the cause.")
 }
 
 
+k <- -1
+t <- -1
 # If there is a manual override, the node number (k) is set to the manual value --------------------------
 if (manualk >= 0) {
   k <- manualk
@@ -95,7 +99,7 @@ if (manualt >= 0) {
 # Verifying that a valid node number and sequence numbers could be allocated manually or automatically
 if (k >= 0 & t >= 0) {
   source("Data_node_core_iter_log-reg.R")
-  data_iter_log_reg(k,t)
+  data_iter_log_reg(manualwd,k,t)
 } else {
   stop("Node numbering was not set properly")
 }
