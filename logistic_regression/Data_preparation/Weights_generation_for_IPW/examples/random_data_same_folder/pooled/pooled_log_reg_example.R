d66b39af28d740137ee6f00469696f8001d14cd7
Jean-Philippe Morissette <jeanp34@hotmail.com>
2025-01-14 17:01:41 -0500

    WIP: Weights & IPW (logistic regression)
    - Began implementation of IPW for logistic regression.
    - New folder (data preparation/weights_generation_for_IPW) was created. Currently, only "random_data_same_folder" has been updated. This could allow for different ways to compute weights.
    - New structure works fine (same results as before), needs to be tested with an actual example.
    - File names might need to be changed so it doesn't get confusing.
    - Still need to make sure predicted weights are computed with the right formula. Might be different if baseline weights are also used.
    -Documentation hasn't been updated yet.

---
+++ logistic_regression/Data_preparation/Weights_generation_for_IPW/examples/random_data_same_folder/pooled/pooled_log_reg_example.R
@@ -0,0 +1,39 @@
+############### Distributed inference ####################
+############### Demo script ##############################
+
+## License: https://creativecommons.org/licenses/by-nc-sa/4.0/
+## Copyright: GRIIS / Université de Sherbrooke
+
+# This assumes three data node files in the same folder named as below
+# The output is visible in RStudio console
+
+# Set working directory automatically
+
+# this.path package is available
+if (require(this.path)) {
+  setwd(this.dir())
+  
+  # else if running in R studio and the rstudioapi is available, set the correct working directory
+} else if ((Sys.getenv("RSTUDIO") == "1") & (require("rstudioapi"))) {
+  print("RSTUDIO")
+  path <- dirname(rstudioapi::getActiveDocumentContext()$path)
+  setwd(path)
+  
+  # no known means to automatically set working directory
+} else {
+  stop("The required conditions to automatically set the working directory are not met. See R file")
+}
+
+# Pooling data for comparison with pooled model
+data_pooled <- rbind(read.csv(paste0("Data_node_1.csv")),
+                     read.csv(paste0("Data_node_2.csv")),
+                     read.csv(paste0("Data_node_3.csv")))
+
+weights_pooled <- read.csv("Weights_pooled.csv")[,1]
+
+# Fitting and printing pooled model
+print("Pooled logistic regression results:")
+fit <- glm(out1 ~ ., data=data_pooled, family="binomial", weights = weights_pooled)
+print(summary(fit)$coefficients)
+print("Confidence intervals")
+print(confint.default(fit))
