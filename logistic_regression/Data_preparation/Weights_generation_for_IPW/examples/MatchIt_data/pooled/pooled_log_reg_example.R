f6ff1df65c5e9f9859cf7a6950ab710a17b2e06f
Jean-Philippe Morissette <jeanp34@hotmail.com>
2025-01-16 15:56:49 -0500

    Examples files for IPW (MatchIt)
    New dataset that allows us to do an example of using IPW as weights in a logistic regression.
    
    This is the initial commit for the data files. Next commit will correct mistakes.

---
+++ logistic_regression/Data_preparation/Weights_generation_for_IPW/examples/MatchIt_data/pooled/pooled_log_reg_example.R
@@ -0,0 +1,47 @@
+############### Distributed inference ####################
+############### Demo script ##############################
+
+## License: https://creativecommons.org/licenses/by-nc-sa/4.0/
+## Copyright: GRIIS / Université de Sherbrooke
+
+# This assumes three data node files in the same folder named as below
+# The output is visible in RStudio console
+
+# Set working directory automatically
+
+# this.path package is available
+if (require(this.path)) {
+  setwd(this.dir())
+  
+  # else if running in R studio and the rstudioapi is available, set the correct working directory
+} else if ((Sys.getenv("RSTUDIO") == "1") & (require("rstudioapi"))) {
+  print("RSTUDIO")
+  path <- dirname(rstudioapi::getActiveDocumentContext()$path)
+  setwd(path)
+  
+  # no known means to automatically set working directory
+} else {
+  stop("The required conditions to automatically set the working directory are not met. See R file")
+}
+
+# Pooling data for comparison with pooled model
+data_pooled <- rbind(read.csv(paste0("Data_node_1.csv")),
+                     read.csv(paste0("Data_node_2.csv")),
+                     read.csv(paste0("Data_node_3.csv")))
+
+# Verifying if weights are available. If not, use values of 1s as uniform weights.
+if (file.exists(paste0("Weights_pooled.csv"))) {
+  weights_pooled <- read.csv("Weights_pooled.csv")[,1]
+} else {
+  weights_pooled <- rep(1, nrow(data_pooled))
+}
+
+# Fitting and printing pooled model
+print("Pooled logistic regression results:")
+fit <- glm(Tx ~ ., data=data_pooled, family="binomial", weights = weights_pooled)
+print(summary(fit)$coefficients)
+print("Confidence intervals")
+print(confint.default(fit))
+
+# Predicted probabilities
+predict(fit, data_pooled, type="response")
