a70165a6e25c61c99dcd8df04bf7eca80e66faf1
Jean-Philippe Morissette <jeanp34@hotmail.com>
2025-01-24 15:06:46 -0500

    Update PoolingOrg_code_lin_reg.R
    - pooled solution now mimics lm command in R.

--- linear_regression/examples/random_data_with_weights_same_folder/pooled/PoolingOrg_code_lin_reg.R
+++ linear_regression/examples/random_data_with_weights_same_folder/pooled/PoolingOrg_code_lin_reg.R
@@ -8,6 +8,7 @@
 
 # Extracts data from the CSV and creates R data frame
 pooled_data <- read.csv("Pooled_data.csv")
+pooled_weights <- read.csv("Weights_pooled.csv")
 
 ## Code assumes a data frame where the first column is the outcome
 ## Creates a data frame with the outcome
@@ -20,19 +21,24 @@ intercept <- rep(1,nrow(pooled_data))
 ## joins the intercepts and the predictors
 intercept_pred <- data.frame(intercept,predictors)
 
+## Code assumes the weights are in the first column
+## Weights will be used in a diagonal matrix
+W <- diag(pooled_weights[,1])
+
 # Summary statistics for the coefficient estimates in linear regression model----
 
-xtx <- t(as.matrix(intercept_pred))%*%as.matrix(intercept_pred)
-xtx_inverse <- solve(xtx)
-yty <- t(as.matrix(outcome))%*%as.matrix(outcome)
-xty <- t(as.matrix(intercept_pred))%*%as.matrix(outcome)
+xtWx <- t(as.matrix(intercept_pred))%*%W%*%as.matrix(intercept_pred)
+xtWx_inverse <- solve(xtWx)
+ytWy <- t(as.matrix(outcome))%*%W%*%as.matrix(outcome)
+xtWy <- t(as.matrix(intercept_pred))%*%W%*%as.matrix(outcome)
+xtW <- t(as.matrix(intercept_pred))%*%W
 
 # Coefficient estimates in linear regression model-------------------------
 
 # Coefficients and Variance matrix
 
-beta <- xtx_inverse%*%xty
-varbeta <- (1/(nrow(pooled_data)-ncol(intercept_pred)))*drop((yty-((t(beta))%*%xty))) * xtx_inverse
+beta <- xtWx_inverse%*%xtWy
+varbeta <- (1/(nrow(pooled_data)-ncol(intercept_pred)))* as.numeric((ytWy - t(beta)%*%xtWy)) * xtWx_inverse
 
 # Confidence interval with alpha=0.05
 
@@ -41,10 +47,12 @@ lower <- beta - qt(p=.05/2, df=nrow(pooled_data)-ncol(intercept_pred), lower.tai
 
 # Summary and outputs ----------------------------------------------------
 
-output <- setNames(data.frame(beta,upper,lower, row.names = c("Intercept",paste0("Pred", c(1:ncol(predictors))))), c("Beta", "Upper", "Lower"))
+output <- setNames(data.frame(beta,lower,upper, row.names = c("Intercept",paste0("Pred", c(1:ncol(predictors))))), c("Beta", "Lower", "Upper"))
 
 ## Producing the CSV file containing the final outputs
 write.csv(output, file="PoolingOrg_results_centralised_lin_reg.csv")
+
+confint(lm(out1~pred1+pred2+pred3+pred4, data = pooled_data, weights = pooled_weights[,1]))
 
 ## Remove all environment variables. 
 ## If you want to see the variable that were create, simply don't execute that line (and clear them manually after)
