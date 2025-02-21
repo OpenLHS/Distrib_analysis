52644b5e248a9fbdf28d4c83af5ce94926cceed1
Jean-Philippe Morissette <jeanp34@hotmail.com>
2025-02-21 14:58:00 -0500

    Copied Case 2 for further investigation.
    Goal is to crash DA.

---
+++ cox_regression/Cox_model/examples/Case2_2_SubGroup_No_Event/distributed/Coord_node_call_iter_cox-reg.R
@@ -0,0 +1,66 @@
+############### DISTRIBUTED COX MODEL ####################
+############### Coordinating node code ###################
+
+## License: https://creativecommons.org/licenses/by-nc-sa/4.0/
+## Copyright: GRIIS / Université de Sherbrooke
+
+# Loading packages and setting up core variables --------------------------
+library("survival")          # Contains the core survival analysis routines
+library(MASS)                # Functions for matrix manipulation (ginv)
+
+# If you want to skip the automated working directory setting, input 1 here. 
+# If you do so, make sure the working directory is set correctly manualy.
+manualwd <- -1
+
+if (manualwd != 1) {
+  
+  # Set working directory automatically
+  
+  # this.path package is available
+  if (require(this.path)) {
+    setwd(this.dir())
+    
+    # else if running in R studio and the rstudioapi is available, set the correct working directory
+  } else if ((Sys.getenv("RSTUDIO") == "1") & (require("rstudioapi"))) {
+    print("RSTUDIO")
+    path <- dirname(rstudioapi::getActiveDocumentContext()$path)
+    setwd(path)
+    
+    # no known means to automatically set working directory
+  } else {
+    stop("The required conditions to automatically set the working directory are not met. See R file")
+  }
+} else {
+  print("The automated working directory setup has been bypassed. If there is an error, this might be the cause.")
+}
+
+# Initialise error messages
+error_message <- NULL
+
+# ------------------------- CODE STARTS HERE ------------------------
+
+# Calculate number of data nodes from files fitting the pattern in the working directory
+# This assumes unique event times outputs have a name like Times_[[:digit:]]+_output.csv
+K=length(list.files(pattern="Times_[[:digit:]]+_output.csv"))
+
+# First step: initialization of times and betas
+if (!file.exists("Global_times_output.csv")) {
+
+  source("Coord_node_init_iter_cox-reg.R")
+  coord_init_iter_cox_reg(manualwd)
+  
+} else {
+  
+  # Find current iteration number
+  files <- list.files(pattern = "Beta_\\d+_output.csv")
+  indext <- as.numeric(gsub("Beta_(\\d+)_output.csv", "\\1", files))
+  t <- max(indext, 0)  
+  t <- t + 1
+  
+  source("Coord_node_add_iter_cox-reg.R")
+  coord_call_add_iter_cox_reg(manualwd, t)
+    
+} 
+
+# Clear variables
+rm(list = ls())
