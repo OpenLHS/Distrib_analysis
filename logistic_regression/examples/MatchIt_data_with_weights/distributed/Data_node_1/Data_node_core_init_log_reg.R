fc415e9b723091d3e035061c883ceda4ff78b430
Jean-Philippe Morissette <jeanp34@hotmail.com>
2025-03-14 15:09:20 -0400

    Final update for merging (IPW within logistic regression)
    - Added our missing value handler.
    - All examples work and provide the same weights (DA vs. pooled)

--- logistic_regression/examples/MatchIt_data_with_weights/distributed/Data_node_1/Data_node_core_init_log_reg.R
+++ logistic_regression/examples/MatchIt_data_with_weights/distributed/Data_node_1/Data_node_core_init_log_reg.R
@@ -36,6 +36,10 @@ if (manualwd != 1) {
   print("The automated working directory setup has been bypassed. If there is an error, this might be the cause.")
 }
   
+  # Handles missing values, if any
+  source("Data_node_core_missingvalues.R")
+  missing_value_handler(man_wd = manualwd, nodeid = k)
+  
   # Expecting data file name like Data_node_1.csv where 1 is the variable k above
   # Construct file name according to node data
   # Assumes default parameters, like header and separator
@@ -87,11 +91,6 @@ if (manualwd != 1) {
     # Last case is when no weights are provided. Uses uniform weights
   } else { 
     node_weights <- rep(1, n)
-  }
-
-  # Method isn't yet available for missing data
-  if(any(is.na.data.frame(node_data))){
-    stop("At least one NA was found in the data. \n The algorithm currently works only with complete data.")
   }
 
   # Makes sure the outcome variable is properly coded as 0s and 1s.
