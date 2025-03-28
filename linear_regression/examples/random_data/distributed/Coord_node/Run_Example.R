c51106e566e12fcfa49451374f07b1f5a2fdf9a3
Jean-Philippe Morissette <jeanp34@hotmail.com>
2025-03-28 16:04:56 -0400

    WIP: Removed file duplication for all example (RL)

--- linear_regression/examples/random_data/distributed/Coord_node/Run_Example.R
+++ linear_regression/examples/random_data/distributed/Coord_node/Run_Example.R
@@ -4,25 +4,18 @@
 ## License: https://creativecommons.org/licenses/by-nc-sa/4.0/
 ## Copyright: GRIIS / Université de Sherbrooke
 
-# Allow to set working directory automatically
+# Set working directory automatically
 library(this.path)
+setwd(this.dir())
 
 # Retrieve current working directory
-setwd(this.dir())
 examplefilepath <- paste0(getwd(), "/")
 
-# Run data node code
-setwd(this.dir())
-source("../../../generic_code/example_handler/Data_node_call_lin-reg_ex.R")
-linear_regression_example_datanode_handler(nodeid = 1, expath = examplefilepath)
-linear_regression_example_datanode_handler(nodeid = 2, expath = examplefilepath)
-
 # Run coordination node code
-setwd(this.dir())
-source("../../../generic_code/example_handler/Coord_node_lin-reg_ex.R")
+source("../../../../generic_code/example_handler/Coord_node_lin-reg_ex.R")
 linear_regression_example_coordnode_handler(expath = examplefilepath)
 
 ## Remove all environment variables. 
 ## If you want to see the variable that were create, simply don't execute that line (and clear them manually after)
 rm(list = ls())
-
+	
