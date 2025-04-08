44ad269cf7b9bbf8d3ef6e3011b3a0b307c88f33
Jean-Philippe Morissette <jeanp34@hotmail.com>
2025-04-08 14:29:40 -0400

    Removed file duplication for all examples (IPW)

--- logistic_regression/Data_preparation/Weights_generation_for_IPW/examples/breast_data/distributed/Coord_node/r_coord_node_iter_log_reg.bat
+++
@@ -1,2 +0,0 @@
-echo off
-rscript Coord_node_call_iter_log-reg.R
---
+++ logistic_regression/Data_preparation/Weights_generation_for_IPW/examples/breast_data/distributed/Data_node_2/Run_Example.R
@@ -0,0 +1,28 @@
+############### DISTRIBUTED LOGISTIC REGRESSION ####################
+
+## License: https://creativecommons.org/licenses/by-nc-sa/4.0/
+## Copyright: GRIIS / Université de Sherbrooke
+
+# Allow to set working directory automatically
+library(this.path)
+
+# If you do not want to use a threshold for the probabilities estimated, input 0 here.
+# Threshold value should be between 0 and 0.5. See details in the instructions.
+manualthresh <- 0.01
+
+# No modifications should be required below this point
+###################################################### 
+
+# Retrieve current working directory
+setwd(this.dir())
+examplefilepath <- paste0(getwd(), "/")
+
+# Run data node code
+setwd(this.dir())
+source("../../../example_handler/Data_node_call_log-reg_ex.R")
+logistic_regression_IPW_example_datanode_handler(nodeid = 2, expath = examplefilepath, man_thresh = manualthresh)
+
+## Remove all environment variables. 
+## If you want to see the variable that were create, simply don't execute that line (and clear them manually after)
+rm(list = ls())
+
