c1401eb7e36eb09a08e25a007c0087255d1d66bf
Jean-Philippe Morissette <jeanp34@hotmail.com>
2025-10-02 10:49:01 -0400

    Update Run_Example.R

--- Vertically_distributed_analysis/logistic_regression_nonpenalized/Logistic_regression_modelling/examples/random_data_same_folder/distributed/Run_Example.R
+++ Vertically_distributed_analysis/logistic_regression_nonpenalized/Logistic_regression_modelling/examples/random_data_same_folder/distributed/Run_Example.R
@@ -11,6 +11,16 @@ library(this.path)
 # Else, an automated value that complies with the assumptions of the method will be assigned.
 lambda <- -1
 
+# If you want to manually set the parameter eta, specify value here.
+# If you do so, please refer to article to ensure adequate settings. 
+# Else, an automated value that complies with the assumptions of the method will be assigned.
+eta <- -1
+
+# If you want to use a specific seed for the privacy check, specify it here.
+# If you do so, all nodes will use the same seed for their privacy check.
+# If commented, no seed will be used
+manualseed <- -1
+
 # No modifications should be required below this point
 ###################################################### 
 
@@ -27,7 +37,7 @@ source("Response_node_call_iter_log-regV_ex.R")
 vert_logistic_regression_example_datanode_handler(nodeid = 2, expath = examplefilepath)
 
 # Run response node code
-vert_logistic_regression_example_coordnode_handler(man_lambda = lambda, expath = examplefilepath)
+vert_logistic_regression_example_coordnode_handler(man_lambda = lambda, man_eta = eta, expath = examplefilepath, man_seed = manualseed)
 
 # Run covariate node code for results
 vert_logistic_regression_example_datanode_handler(nodeid = 2, expath = examplefilepath)
